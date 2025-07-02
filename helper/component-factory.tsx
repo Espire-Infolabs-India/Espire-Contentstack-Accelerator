dcfdb1b31e04b3c358d12b61ff82580fd0223393
Atul_Gusain <atul.gusain@espire.com>
2025-07-02 16:40:54 +0530

    Code changes for Carousel Implementation

--- helper/component-factory.tsx
+++ helper/component-factory.tsx
@@ -2,10 +2,12 @@ import HeroBanner from "../components/hero-banner";
 import Promo from "../components/promo";
 import CTA from "../components/cta";
 import BlogListing from "../components/bloglisting";
+import Carousel from "../components/carousel";
 
 export const ComponentMap: Record<string, (data: any) => JSX.Element> = {
   hero_banner: (data) => <HeroBanner {...data} />,
   promo: (data) => <Promo {...data} />,
   cta: (data) => <CTA {...data} />,
   blog_listing: () => <BlogListing />,
+  carousel: (data) => <Carousel {...data} />,
 };
