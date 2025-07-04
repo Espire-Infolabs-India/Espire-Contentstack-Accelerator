933ed2b520e3b13322d7715850bd907551f9569a
Atul_Gusain <atul.gusain@espire.com>
2025-07-04 14:24:37 +0530

    changes in component factory added some new component.

--- helper/component-factory.tsx
+++ helper/component-factory.tsx
@@ -2,10 +2,20 @@ import HeroBanner from "../components/hero-banner";
 import Promo from "../components/promo";
 import CTA from "../components/cta";
 import BlogListing from "../components/bloglisting";
+import Seperator from "../components/seperator";
+import SocialMediaShare from "../components/social-media-share";
+import LinkList from "../components/link-list";
 
 export const ComponentMap: Record<string, (data: any) => JSX.Element> = {
   hero_banner: (data) => <HeroBanner {...data} />,
   promo: (data) => <Promo {...data} />,
   cta: (data) => <CTA {...data} />,
   blog_listing: () => <BlogListing />,
+  seperator: () => <Seperator />,
+  social_media_share: (data) => (
+    <SocialMediaShare items={data?.social_media_share || []} />
+  ),
+  link_list: (data) => (
+    <LinkList title={data?.title as string} links={data?.link_list || []} />
+  ),
 };
