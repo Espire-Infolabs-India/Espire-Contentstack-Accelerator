16967f7b59ddc0af69585f7f8152b8381e50cdf0
Atul_Gusain <atul.gusain@espire.com>
2025-07-07 14:37:02 +0530

    script added in layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode, useState, useEffect } from "react";
 import { Page } from "../model/page.model";
 import Header from "./header";
 import Footer from "./footer";
+import Head from "next/head";
 
 type Props = {
   children?: ReactNode;
@@ -11,12 +12,27 @@ type Props = {
   entries: Page[];
 };
 
-export default function Layout({ children, page, header, footer, entries }: Props) {
+export default function Layout({
+  children,
+  page,
+  header,
+  footer,
+  entries,
+}: Props) {
   let jsonPreview = {};
   if (page) jsonPreview["page"] = page;
 
   return (
     <>
+      <Head>
+        <script src="https://web.cmp.usercentrics.eu/modules/autoblocker.js"></script>
+        <script
+          id="usercentrics-cmp"
+          src="https://web.cmp.usercentrics.eu/ui/loader.js"
+          data-settings-id="WsB9MfwJE5J5uE"
+          async
+        ></script>
+      </Head>
       <Header data={header} />
       {children}
       <Footer data={footer} />
