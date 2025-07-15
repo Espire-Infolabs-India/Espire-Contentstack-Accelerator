d77d89d9e4ca8b128e80ea6dbbdde5101a7fcf18
Prashant Tomar <prtomar_VENDOR@acu.edu.au>
2025-07-15 13:14:08 +0530

    fixing the parsing

--- components/plainhtml.tsx
+++ components/plainhtml.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { PlainHtmlProps } from "../model/component-props/plain-html.model";
+import parse from "html-react-parser";
 
 export default function PlainHtmlComponent({ plain_html }: PlainHtmlProps) {
   if (!plain_html) return null;
 
-  return (
-    <div dangerouslySetInnerHTML={{ __html: plain_html }} />
-  );
+  return <>{parse(plain_html)}</>;
 }
