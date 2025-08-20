46414fc97fab0dd9f1bc2fb9411f703272573be9
ESPIRE\vibha.agarwal <vibha.agarwal@espire.com>
2025-08-20 17:22:07 +0530

    feature-cards alignment issue fix

--- components/featurehighlights.tsx
+++ components/featurehighlights.tsx
@@ -24,7 +24,7 @@ const FeatureHighlightComponent = (data: FeatureHighlights) => {
           </div>
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {data?.featured_post?.map((post, index) => (
             <div
               key={index}
